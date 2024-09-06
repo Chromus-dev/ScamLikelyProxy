f5e3ac97333ea99854a22a7bfe8392859290a5d7
Chromus <chasecrampton9@gmail.com>
2024-09-06 20:35:10 +0000

    server should work hopefully

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.get("/", (req, res) => {
    res.send("This is home page.");
 });
 
+app.get('/:link', (req, res) => {
+    let link = req.params.link
+    fetch('https://link-checker.nordvpn.com/v1/public-url-checker/check-url', {
+        method: 'POST',
+        body: JSON.stringify({url: link})
+    })
+    .then(res => res.json())
+    .then(data => res.send(data))
+})
+
 app.post("/", (req, res) => {
    res.send("This is home page with post request.");
 });
